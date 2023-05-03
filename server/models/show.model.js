607a4a4912e9394b3a90f0ff136efffa360bb99a
Christian Johnson <charcoaldust@mac.com>
2023-05-03 14:33:36 -0700

    added show model

---
+++ server/models/show.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ShowSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    genre: { type: String },
+    release_year: { type: Number },
+  },
+  { timestamps: true }
+);
+
+const Show = mongoose.model("Show", ShowSchema);
+
+module.exports = Show;
